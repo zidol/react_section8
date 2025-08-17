dc1b7837d86cd9c666513a2d527c7f3cd5754299
zidol <ajh3166@naver.com>
2025-08-17 21:22:59 +0900

    Feat : useMemo와 연산 최적화

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,10 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 const List = ({ todos, onUpdate, onDeleate }) => {
   const [search, setSearch] = useState("");
 
   const onChangeSearch = (e) => {
-    console.log(e.target.value);
     setSearch(e.target.value);
   };
 
@@ -20,9 +19,24 @@ const List = ({ todos, onUpdate, onDeleate }) => {
 
   const filteredDotos = getFilteredData();
 
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    console.log("getAnalyzeData 호출!!");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+    return { totalCount, doneCount, notDoneCount };
+  }, [todos]);
+  //의존성 배열 : deps
+
+  // const { totalCount, doneCount, notDoneCount } = getAnalyyzeData();
   return (
     <div className="List">
       <h4>Todo List🌱</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount} </div>
+        <div>notDone:{notDoneCount}</div>
+      </div>
       <input
         value={search}
         onChange={onChangeSearch}
